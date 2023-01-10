e82ed682f5be0ca7957fa513c12ae86b536f8d2b
Aloxan12 <kvakvaaak@mail.ru>
2023-01-10 13:49:52 +0300

    layout component

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,7 +1,16 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import Heading from "./components/Heading";
+import {useRouter} from "next/router";
 
 const Error = () => {
+    const {push} = useRouter()
+
+    useEffect(()=>{
+        setTimeout(()=>
+            push('/')
+        ,5000)
+    },[])
+
     return (
         <div>
             <Heading text={'404'} />
