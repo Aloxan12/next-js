c3b4bf6ca64bfde760b9fb0408362825b2e1e7c3
Aloxan12 <kvakvaaak@mail.ru>
2023-01-10 11:22:06 +0300

    routing

---
+++ pages/contacts/index.tsx
@@ -0,0 +1,6 @@
+const Contacts =()=>{
+    return <>
+        <h1>Contacts list:</h1>
+    </>
+}
+export default Contacts
---
+++ pages/posts.tsx
@@ -0,0 +1,9 @@
+
+export const Posts = ()=>{
+  return <div>
+    <h1>Post lists:</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing.</p>
+  </div>
+}
+
+export default Posts
