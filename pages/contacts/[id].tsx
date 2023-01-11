f8cb8b99435b375793bb28dabf1bea82a065e054
Aloxan12 <kvakvaaak@mail.ru>
2023-01-11 15:08:29 +0300

    fetch

--- pages/contacts/[id].tsx
+++ pages/contacts/[id].tsx
@@ -1,6 +1,20 @@
 import Heading from "../components/Heading";
 import ContactInfo from "../components/ContactInfo";
 
+export const getServerSideProps = async (context: any) => {
+    const { id } = context.params
+    const response = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
+    const data = await response.json()
+    if (!data) {
+        return {
+            notFound: true
+        }
+    }
+    return {
+        props: {contact: data}
+    }
+}
+
 interface IContact{
     contact: any
 }
