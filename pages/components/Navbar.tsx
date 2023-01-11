e9425fb447a014a4a5102b8098117bf52d8e5abc
Aloxan12 <kvakvaaak@mail.ru>
2023-01-11 14:27:17 +0300

    layout component

--- pages/components/Navbar.tsx
+++ pages/components/Navbar.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link'
 import styles from '../../styles/Navbar.module.scss'
 import {useRouter} from "next/router";
+import Image from "next/image";
 
 const navigation = [
     {id: 1, title: 'Home', path: '/'},
@@ -11,7 +12,9 @@ const navigation = [
 const Navbar = () => {
     const { pathname } = useRouter()
     return <nav className={styles.nav}>
-        <div>Next navbar</div>
+        <div className={styles.logo}>
+            <Image width={20} height={20} src='/img.png' alt={'logo'}/>
+        </div>
         <div className={styles.links}>
             {navigation.map(({id, title, path}) => (
                 <Link href={path} key={id} className={pathname === path ? styles.active : '' }>{title}</Link>
