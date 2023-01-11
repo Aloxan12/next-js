a4194c5d4aec5f58293172ee59ab1ba7f03d07f8
Aloxan12 <kvakvaaak@mail.ru>
2023-01-11 14:34:02 +0300

    fetch

--- pages/contacts/index.tsx
+++ pages/contacts/index.tsx
@@ -1,6 +1,20 @@
 import Heading from "../components/Heading";
+import {useEffect, useState} from "react";
+
+const getStaticProps = async ()=>{}
 
 const Contacts =()=>{
+    const [contacts, setContacts] = useState<any[]>([])
+
+    useEffect(()=>{
+        const fetchData = async ()=>{
+            const response = await fetch('https://jsonplaceholder.typicode.com/users')
+            const data = await response.json()
+            setContacts(data)
+        }
+        fetchData()
+    },[])
+
     return <>
         <Heading text={'Contacts list:'}/>
     </>
